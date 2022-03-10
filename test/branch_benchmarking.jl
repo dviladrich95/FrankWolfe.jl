7d849127a851471125231b6c7d1d1d1e643b57d2
dviladrich95 <daniel.viladrich.h@gmail.com>
2022-03-10 16:49:31 +0100

    added invokelatest here also

--- test/branch_benchmarking.jl
+++ test/branch_benchmarking.jl
@@ -6,12 +6,12 @@ using SparseArrays
 using LibGit2
 import FrankWolfe: ActiveSet
 
-
 function get_include(dir)
     path = joinpath(dir,"test/benchmarking_suite.jl")
     function run_include()
         include(path)
-        return run_benchmark()
+        run_benchmark_latest = Base.invokelatest(run_benchmark)
+        return run_benchmark_latest()
     end
 end
 
